00192ddd88a1e370df3b703fd8e789ed25b3febf
AitoTonttu <killermasa95@gmail.com>
2016-05-09 20:26:19 +0300

    Update random_flash_main.lua

--- sample-lua-scripts/random_flash_main.lua
+++ sample-lua-scripts/random_flash_main.lua
@@ -7,23 +7,23 @@ function play_anim()
 		c = Colore.Color.White
 		background = Colore.Color.Black
 		-- set keyboard colour
-		Keyboard.SetPosition(math.random(0,5), math.random(1,18), c, true)
+		Keyboard.SetPosition(math.random(1,0), math.random(1,18), c, true)
 		-- set mousepad colour
 		Mousepad.SetAll(background)
-		Mousepad[math.random(0,15)] = c
-		Mousepad[math.random(0,15)] = c
-		Mousepad[math.random(0,15)] = c
+		Mousepad[math.random(0,5)] = c
+		Mousepad[math.random(0,5)] = c
+		Mousepad[math.random(0,5)] = c
 		-- set mouse colour
 		Mouse.SetAll(background)
-		Mouse[math.random(0,9), math.random(0,7)] = c
-		Mouse[math.random(0,9), math.random(0,7)] = c
-		Mouse[math.random(0,9), math.random(0,7)] = c
+		Mouse[math.random(1,2), math.random(0,9)] = c
+		Mouse[math.random(1,2), math.random(0,9)] = c
+		Mouse[math.random(1,2), math.random(0,9)] = c
 	
 		-- set keypad colour
 		Keypad.SetAll(background)
-		Keypad[math.random(0,4),math.random(0,5)] = c
-		Keypad[math.random(0,4),math.random(0,5)] = c
-		Keypad[math.random(0,4),math.random(0,5)] = c
+		Keypad[math.random(1,0),math.random(0,8)] = c
+		Keypad[math.random(1,0),math.random(0,8)] = c
+		Keypad[math.random(1,0),math.random(0,8)] = c
 
 		-- We don't want to spam the SDK, so throttle to 50ms
 		Thread.Sleep(50)
